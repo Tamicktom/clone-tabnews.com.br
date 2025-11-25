ad6bbadf7f348ca25aac49594b3fcabb50e08530
Henrique Fonseca <60244227+Tamicktom@users.noreply.github.com>
2025-11-25 00:39:23 +0000

    feat: add environment validation using zod for database and server configuration

---
+++ env/server.ts
@@ -0,0 +1,17 @@
+//* Libraries imports
+import z from 'zod';
+
+const databasetEnvSchema = z.object({
+  POSTGRES_HOST: z.string().min(1, 'POSTGRES_HOST is required'),
+  POSTGRES_PORT: z.coerce.number().default(5432),
+  POSTGRES_USER: z.string().min(1, 'POSTGRES_USER is required'),
+  POSTGRES_PASSWORD: z.string().min(1, 'POSTGRES_PASSWORD is required'),
+  POSTGRES_DB: z.string().min(1, 'POSTGRES_DB is required'),
+});
+
+const serverEnvSchema = z.object({
+  ...databasetEnvSchema.shape,
+  PORT: z.coerce.number().default(3000),
+});
+
+export const env = serverEnvSchema.parse(process.env);
